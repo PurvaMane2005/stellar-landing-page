648ecccd73396770fed60e85b487c7224edcc55a
PurvaMane2005 <purvamane2005@gmail.com>
2025-06-09 18:25:35 +0530

    second iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import Orb from "./components/Orb";
-import SplitText from "./components/SplitText"; // or './SplitText' if that's the filename
-
+import Orb from "./components/Orb/Orb";
+import SplitText from "./components/SplitText/SplitText"; // or './SplitText' if that's the filename
+import ScrollVelocity from './components/ScrollVelocity/ScrollVelocity'; // or './
+import CardSwap, { Card } from './components/AIAgent/CardSwap'; // or './CardSwap' if that's the filename
 const App = () => {
   const handleAnimationComplete = () => {
     console.log("All letters have animated!");
@@ -9,14 +10,23 @@ const App = () => {
 
   return (
     <div className="app-container">
-      <div className="orb-wrapper">
+      {/* <div className="orb-wrapper">
         <Orb 
           hoverIntensity={0.5}
           rotateOnHover={true}
           hue={0}
           forceHoverState={false}/>
-      </div>
-      <div>
+      </div> */}
+      
+      {/* <div style={{ marginTop: '40rem' }}>
+            <ScrollVelocity
+            texts={['React Bits', 'Scroll Down']} 
+            velocity = {60} 
+            className="custom-scroll-text"
+          />
+          </div> */}
+
+      {/* <div>
           <SplitText
             text="Hello, world!"
             className="text-2xl font-semibold"
@@ -31,7 +41,55 @@ const App = () => {
             textAlign="center"
             onLetterAnimationComplete={handleAnimationComplete}
           />
-        </div>
+          
+        </div> */}  
+      
+
+      <div style={{ height: '600px', position: 'relative' }}>
+        <CardSwap
+          cardDistance={60}
+          verticalDistance={70}
+          delay={5000}
+          pauseOnHover={false}
+          skewAmount={6}
+          width={900}
+          height={700}
+          easing="elastic"
+        >
+        <Card className="card">
+            <h3 style={{ margin: "10px", color: "white", textAlign: "left", paddingLeft: "10px" }}>
+              Slide 1
+            </h3>
+            <img
+              src="https://integralwebsolution.com/wp-content/uploads/2020/03/custom-template-banner3.jpg"
+              alt="Slide 1"
+              style={{ width: "100%", padding: "20px", borderRadius: "12px 12px 12px 12px" }}
+            /> 
+        </Card>
+
+      <Card>
+        <h3 style={{ margin: "10px", color: "white", textAlign: "left", paddingLeft: "10px" }}>
+          Slide 2
+        </h3>
+        <img
+          src="https://assets.website-files.com/5c75b94c8dd1ae50d3b9294b/5d12756a077913dd4f464c83_n.png"
+          alt="Slide 2"
+          style={{ width: "100%", padding:"20px", borderRadius: "12px 12px 0 0" }}
+        />
+      </Card>
+
+      <Card>
+        <h3 style={{ margin: "10px", color: "white", textAlign: "left", paddingLeft: "10px" }}>
+          Slide 3
+        </h3>
+        <img
+          src="https://freedesignresources.net/wp-content/uploads/2019/03/Web-Showcase-Mockup-Creator_Graphic-Gata_211017_prev01.jpg"
+          alt="Kitten 3"
+          style={{ width: "100%", padding:"20px", borderRadius: "12px 12px 0 0" }}
+        />
+      </Card>
+        </CardSwap>
+      </div>
     </div>
   );
 };
